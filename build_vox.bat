2d36cf3ab237b8acffcde61d297ccf37e929f962
Bertrand Reyna-Brainerd <bertbrainerd@gmail.com>
2026-03-22 23:06:02 -0400

    chore: remove scratch files from tracking, extend .gitignore

--- build_vox.bat
+++
@@ -1,4 +0,0 @@
-@echo off
-call "C:\Program Files\Microsoft Visual Studio\2022\Community\VC\Auxiliary\Build\vcvars64.bat"
-cargo build --release -p vox-cli --features gpu,populi-candle-cuda,populi-dei
-exit /b %errorlevel%
